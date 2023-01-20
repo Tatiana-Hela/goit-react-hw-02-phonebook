be69d8a8ef56207098a54a94da7d99428481e697
Tatiana-Hela <malya9472@gmail.com>
2023-01-20 17:28:40 +0300

    add contactForm

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,10 +2,17 @@ import { Component } from 'react';
 import { nanoid } from 'nanoid';
 import css from '../App/App.module.css';
 
+import ContactForm from 'components/ContactForm/ContactForm';
+
 class App extends Component {
   state = {
-    contacts: [],
-    name: '',
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
   };
 
   addNewContact = (name, number) => {
@@ -40,25 +47,10 @@ class App extends Component {
     return (
       <>
         <h2 className={css.title}>Phonebook</h2>
-        <form className={css.form}>
-          <label className={css.label}>
-            Name
-            <input
-              className={css.input}
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-            />
-          </label>
-          <button className={css.button} type="submit">
-            Add contacts
-          </button>
-        </form>
+        <ContactForm onSubmit={this.addNewContact} />
         <h2 className={css.title}>Contacts</h2>
         <ul>
-          <li>{this.addNewContact}</li>
+          <li></li>
         </ul>
       </>
     );
